d714dc0079ebf0052f7ec4dfdf4a09c60cfefb24
shuvo2k <findariful@gmail.com>
2018-07-01 22:54:30 +0600

    topic forms added

--- boards/tests.py
+++ boards/tests.py
@@ -52,3 +52,21 @@ class BoardTopicsTests(TestCase):
 		self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
 
+class NewTopicTests(TestCase):
+	def setUp(self):
+		Board.objects.create(name='Django', description='Django board.')
+
+	def test_new_topic_view_success_status_code(self):
+		url = reverse('new_topic', kwargs={'pk':1})
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+
+	def test_new_topic_view_not_found_status_code(self):
+		url = reverse('new_topic', kwargs={'pk':99})
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 404)
+
+	def test_new_topic_url_resolves_new_topic_view(self):
+		view = resolve('/boards/1/new')
+		self.assertEquals(view.func, new_topic)
+		
