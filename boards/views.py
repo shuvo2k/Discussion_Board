f2e31485564f21b568258f53ce1c3bfd8b1fede4
shuvo2k <findariful@gmail.com>
2018-07-09 22:20:28 +0600

    new topics added

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Board
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User 
 # Create your views here.
 
 def home(request):
@@ -14,6 +15,27 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
 	board = get_object_or_404(Board, pk=pk)
+
+	if request.method == 'POST':
+		subject = request.POST['subject']
+		message = request.POST['message']
+
+		user = User.objects.first()
+
+		topic = Topic.objects.create(
+			subject=subject,
+			board=board,
+			starter=user
+		)
+
+		post = Post.objects.create(
+			message=message,
+			topic=topic,
+			created_by=user
+		)
+
+		return redirect('board_topics', pk=board.pk)
+
 	return render(request, 'new_topic.html', {'board':board})
 
 
